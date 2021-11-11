6b9102c9755a0f68c26b9cf06b913e9a41d08df9
Miquel <miqueldevweb@gmail.com>
2021-11-12 00:09:50 +0100

    Fi Nivell 3

---
+++ Nivell3/exercici1.php
@@ -0,0 +1,24 @@
+<?php
+//Este ejercicio lo venía haciendo de otra manera, he encontrado esta solución en internet y no la había visto, me parece más eficiente que la mia asi que la he estudiado y puesto en práctica. Se parte de la base de que los numeros buscados son los indices de un array booleano, true/false indicaran si el indice es primo o no.
+
+$userNumber = 800;
+
+for($i = 0; $i < $userNumber; $i++){    //Inicializa el array, se necesita que todos los valores sean TRUE para que mas adelante se comprueben uno por uno.
+  $numbers[$i] = true;
+}
+
+
+for ($n = 2; $n < $userNumber; $n++){   //El primer numero primo es el 2 asi que empezamos a recorrer el array desde el indice 2.
+  if ($numbers[$n]){    //Si el numero esta marcado como primo recorremos los múltiplos hasta el numero objetivo. Por eso hemos inicializado todos los elementos con TRUE.
+    for ($i = $n * $n ;$i < $userNumber; $i += $n){ //Recorre todos los multiplos del numero primo hasta el objetivo. Idea base de la criba de Eratostenes.
+       $numbers[$i] = false;
+    }
+  }
+}
+
+for ($n = 2; $n < $userNumber; $n++){
+  if ($numbers[$n]){    //En mi algoritimo manipulaba los elementos del array asignado 0 a los elementos no primos. Este metodo es claramente mejor.
+    echo $n." ";
+  }
+}
+?>
