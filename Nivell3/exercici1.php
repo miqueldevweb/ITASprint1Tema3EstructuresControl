7a8a02a01f63c1986f47b16cb81e6db3d8688b5a
Miquel <miqueldevweb@gmail.com>
2021-11-12 08:21:24 +0100

    Se añaden comentarios al exercici1 Nivell3

--- Nivell3/exercici1.php
+++ Nivell3/exercici1.php
@@ -1,6 +1,6 @@
 <?php
 //Este ejercicio lo venía haciendo de otra manera, he encontrado esta solución en internet y no la había visto, me parece más eficiente que la mia asi que la he estudiado y puesto en práctica. Se parte de la base de que los numeros buscados son los indices de un array booleano, true/false indicaran si el indice es primo o no.
-
+//Mi consulta por internet viene de la necesidad de algún tipo de lista doblemente enlazada, el array visto hasta ahora no lo es. En la documentación encontrada he visto que se utiliza una BBDD como lista, cuestión que queda para temas posteriores.
 $userNumber = 800;
 
 for($i = 0; $i < $userNumber; $i++){    //Inicializa el array, se necesita que todos los valores sean TRUE para que mas adelante se comprueben uno por uno.
